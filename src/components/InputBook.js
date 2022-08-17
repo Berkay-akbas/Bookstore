f6c752c059ac508e2f6953e2d3e0c6afe4e8d227
Berkay Akbas <berkayakbas@gmail.com>
2022-08-17 14:52:05 +0300

    Implement removing and adding books, status check for categories page

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -1,15 +1,29 @@
-import React from 'react';
-// import { addBook } from '../redux/books/books';
+import { React, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const InputBook = (bookProps) => {
   // eslint-disable-next-line
   const adds = bookProps.addBookProps;
-  // console.log(bookProps);
+
+  const [book, setBook] = useState({ title: '', author: '' });
+
+  const bookInput = (e) => {
+    e.preventDefault();
+    setBook({ ...book, [e.target.name]: e.target.value });
+  };
+
+  const addBook = (e) => {
+    e.preventDefault();
+    const bookItem = { id: uuidv4(), title: book.title, author: book.author };
+    adds(bookItem);
+    setBook({ title: '', author: '' });
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="Book Title" />
-      <input type="text" placeholder="Author" />
-      <button type="button" onClick={() => adds({ book: { id: 4, title: 'asd', author: 'bl' } })}>Add Book</button>
+    <form onSubmit={addBook}>
+      <input type="text" placeholder="Book Title" onChange={bookInput} value={book.title} name="title" />
+      <input type="text" placeholder="Author" onChange={bookInput} value={book.author} name="author" />
+      <button type="submit">Add Book</button>
     </form>
   );
 };
